b088172b9ddbc651224304fb50855fcd6d5e6449
Vitalii <vitaliyfilarskiy@gmail.com>
2025-06-19 13:29:17 +0200

    excs 1

--- week1/main2.py
+++ week1/main2.py
@@ -1,16 +1,16 @@
-alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-messsage = 'Brutte is killer!!!'
-key = 3
-messsage = messsage.upper()
-cipher = ''
+def psd(messsage,key):
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    messsage = messsage.upper()
+    cipher = ''
 
-for i in messsage:
-    if i in alpha:
-        index = (alpha.find(i) + key) % len(alpha)
+    for i in messsage:
+        if i in alpha:
+            index = (alpha.find(i) + key) % len(alpha)
 
-        cipher += alpha[index]
-    else:
-        cipher += i
+            cipher += alpha[index]
+        else:
+            cipher += i
+    print(cipher)
 
-
-print(cipher)
+psd('booom boom',6)
+psd('HUUUS HUUS',-6)
